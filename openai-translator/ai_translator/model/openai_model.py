6e9105eb8ea08e6920e16494bd8b93d368b1c0c7
Leo Zhang <yzhang252@dxc.com>
2024-12-31 10:38:52 +0800

    dev

--- openai-translator/ai_translator/model/openai_model.py
+++ openai-translator/ai_translator/model/openai_model.py
@@ -17,7 +17,7 @@ class OpenAIModel(Model):
         attempts = 0
         while attempts < 3:
             try:
-                if self.model == "gpt-3.5-turbo":
+                if self.model == "gpt4o":
                     response = self.client.chat.completions.create(
                         model=self.model,
                         messages=[
